1f40b092c22f53926d63282b39162b0335b23a21
mohamedhelal <mhelal566@yahoo.com>
2024-08-18 00:53:36 +0300

    add injection file

---
+++ lib/injection.dart
@@ -0,0 +1,64 @@
+import 'package:flutter_clean_architecture/core/network/network_info.dart';
+import 'package:flutter_clean_architecture/features/posts/data/datasources/post_local_data_source.dart';
+import 'package:flutter_clean_architecture/features/posts/data/datasources/post_remote_data_source.dart';
+import 'package:flutter_clean_architecture/features/posts/data/repostiroies/post_repostories_impl.dart';
+import 'package:flutter_clean_architecture/features/posts/domain/repositories/post_repostiroieas.dart';
+import 'package:flutter_clean_architecture/features/posts/domain/usecases/add_post.dart';
+import 'package:flutter_clean_architecture/features/posts/domain/usecases/delete_post.dart';
+import 'package:flutter_clean_architecture/features/posts/domain/usecases/get_all_posts.dart';
+import 'package:flutter_clean_architecture/features/posts/domain/usecases/update_post.dart';
+import 'package:flutter_clean_architecture/features/posts/presentation/bloc/add_delete_update/add_delete_update_post_cubit.dart';
+import 'package:flutter_clean_architecture/features/posts/presentation/bloc/getPost_cubit/posts_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  /// features posts
+// Bloc
+  sl.registerFactory(
+    () => PostsCubit(sl()),
+  );
+  sl.registerFactory(
+    () => AddDeleteUpdatePostCubit(sl(), sl(), sl()),
+  );
+//use cases
+  sl.registerLazySingleton(
+    () => (GetAllPostsUseCases(sl())),
+  );
+  sl.registerLazySingleton(
+    () => DeletePost(sl()),
+  );
+  sl.registerLazySingleton(
+    () => UpdatePost(sl()),
+  );
+  sl.registerLazySingleton(
+    () => AddPost(sl()),
+  );
+// Repostories
+  sl.registerLazySingleton<PostRepostiroieas>(() => PostsRepostoriesImpl(
+      remoteDataSource: sl(), localDataSource: sl(), networkinfoImpl: sl()));
+// Data Sources
+  sl.registerLazySingleton<PostRemoteDataSource>(
+    () => PostRemoteDataSourceImpl(sl()),
+  );
+  sl.registerLazySingleton<PostLocalDataSource>(
+    () => PostLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+
+  /// core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkinfoImpl(sl()),
+  );
+
+  /// External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(
+    () => http.Client(),
+  );
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/core/theme/app_theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture/features/posts/presentation/bloc/add_delete_update/add_delete_update_post_cubit.dart';
+import 'package:flutter_clean_architecture/features/posts/presentation/bloc/getPost_cubit/posts_cubit.dart';
+import 'injection.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'posts',
-            style: TextStyle(color: Colors.white),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PostsCubit>(
+          create: (_) => di.sl<PostsCubit>(),
+        ),
+        BlocProvider<AddDeleteUpdatePostCubit>(
+          create: (_) => di.sl<AddDeleteUpdatePostCubit>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'posts',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          body: Container(
+            child: const Center(
+              child: Text("Hellp clean"),
+            ),
           ),
         ),
-        body: Container(
-          child: Center(
-            child: Text("Hellp clean"),
-          ),
-        ),
+        theme: appTheme,
       ),
-      theme: appTheme,
     );
   }
 }
